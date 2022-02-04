8fb94bdf74683ad42824f05026c40849caa033ed
unknown <BT_1E10_14@BKTECH.LOCAL>
2022-02-04 11:22:14 -0500

    Initial commm

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> trees= new ArrayList<String>() ;
+        trees. add("pine");
+        trees. add("maple");
+        trees.add("frasier");
+        System.out.println(trees);
+        System.out.println(trees.size());
+
+    }
+}
