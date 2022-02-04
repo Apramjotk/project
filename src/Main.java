671f8e75f207420192e45b0d1472647d80d3c307
BT_1E10_14 <apramjotk@gmail.com>
2022-02-04 11:32:04 -0500

    hello

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> trees= new ArrayList<String>() ;
-        trees. add("pine");
-        trees. add("maple");
+        ArrayList<String> trees= new ArrayList<String>();
+        trees.add("pine");
+        trees.add("maple");
         trees.add("frasier");
         System.out.println(trees);
         System.out.println(trees.size());
+        System.out.println("Hello");
 
     }
 }
